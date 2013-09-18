de606cce43966dd564d1a221cc8a60fe89e9abbd
Ag <github@assembledgoods.com>
2013-09-17 20:15:18 -0500

    Chain scopes instead of merging in Spree::ProductScope#apply_on

--- app/models/spree/product_group.rb
+++ app/models/spree/product_group.rb
@@ -145,7 +145,8 @@ module Spree
     end
 
     def include?(product)
-      res = apply_on(Product.where("spree_products.id = ?", product.id), false)
+      # res = apply_on(Product.where("spree_products.id = ?", product.id), false)
+      res = apply_on(Product.where(:id => product.id), false) 
       res.length > 0
     end
 
